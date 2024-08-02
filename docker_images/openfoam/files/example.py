1656dc3f47317aa70285ecd9d04f2e08915f275e
Matt Field <matthew.field@ukaea.uk>
2024-08-02 16:52:25 +0100

    Updated docker image for FDS

--- docker_images/openfoam/files/example.py
+++ docker_images/openfoam/files/example.py
@@ -1,4 +1,4 @@
-from simvue_integrations.wrappers.openfoam import OpenfoamRun
+from simvue_integrations.connectors.openfoam import OpenfoamRun
 
 with OpenfoamRun() as run:
     # Initialize your Simvue run as normal
